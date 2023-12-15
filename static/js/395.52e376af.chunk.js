3a91195ad4f3869b6a517c99b0efbf6f756c98d9
Dmytro-Lys <121360853+Dmytro-Lys@users.noreply.github.com>
2023-12-15 23:11:14 +0000

    Deploying to gh-pages from @ Dmytro-Lys/novaposhta-advanced@fb3dfcc3e15767db47423e597934f4ad72131fdc 🚀

---
+++ static/js/395.52e376af.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[395],{7395:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h2",{children:"Favorites Cars"})})}}}]);
+//# sourceMappingURL=395.52e376af.chunk.js.map

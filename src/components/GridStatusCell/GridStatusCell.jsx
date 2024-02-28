bdb5a561c0a02fbf01fc28a0e392422da64539a6
Dmytro-Lys <d.lysenko122@gmail.com>
2024-02-28 16:47:34 +0200

    fix bag first render in github for GridStatusCell

--- src/components/GridStatusCell/GridStatusCell.jsx
+++ src/components/GridStatusCell/GridStatusCell.jsx
@@ -1,21 +1,21 @@
 import clsx from "clsx"
 
 import { useDispatch } from "react-redux";
-import { useRef, useEffect } from "react";
+import {  useEffect } from "react";
 import { fetchDocumentStatus } from 'API/novaposhtaAxiosAPI';
 import css from './GridStatusCell.module.css'
 
 const GridStatusCell = ({ text, Number }) => {
     const dispatch = useDispatch()
-    const firstRender = useRef(true)
+    // const firstRender = useRef(true)
   const { REACT_APP_TRACKING_API_KEY } = process.env;
 
     useEffect(() => {
-        if (firstRender.current) {
-            firstRender.current = false
-            return
-        }
-      if (!REACT_APP_TRACKING_API_KEY) return
+        // if (firstRender.current) {
+        //     firstRender.current = false
+        //     return
+        // }
+      if (!REACT_APP_TRACKING_API_KEY) return console.log('TRACKING_API_KEY is not read')
       if (text === 'У дорозі' || text === 'Готується до відправлення' || text === 'Змінено адресу') {
         dispatch(fetchDocumentStatus({
           apiKey: REACT_APP_TRACKING_API_KEY,

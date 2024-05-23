bd34395258fd08a5a9ac336fe280b6a7ccedf096
Dmytro-Lys <d.lysenko122@gmail.com>
2024-05-23 15:35:34 +0300

    localization labels pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import TablePagination from '@mui/material/TablePagination';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ukUA } from '@mui/material/locale';
+
 import { getLimit, getPage } from 'redux/query/selectors';
 import { useSelector, useDispatch } from "react-redux";
 import { setLimit, setPage } from 'redux/query/querySlice';
 import { getDocumentsCount } from 'redux/documents/selectors';
+
+
 
 
 const Pagination = () => {
@@ -18,8 +23,15 @@ const Pagination = () => {
       dispatch(setLimit(parseInt(event.target.value, 10)))  
       dispatch(setPage(1))     
     };
-    
+
+    const theme = createTheme(
+      ukUA
+    );
+    // const defaultLabelDisplayedRows = ({ from, to, count }) =>
+    // { return `${from}–${to} з ${count !== -1 ? count : `більше ніж ${to}`}`; }
+
     return (
+      <ThemeProvider theme={theme}>
         <TablePagination
             component="div"
             count={count}
@@ -27,9 +39,11 @@ const Pagination = () => {
             onPageChange={handleChangePage}
             rowsPerPage={limit}
             rowsPerPageOptions={[5, 10, 20, 50, 100]}
-            labelRowsPerPage ='Елементів на сторінці'
+            // labelRowsPerPage='Елементів на сторінці'
+            // labelDisplayedRows={defaultLabelDisplayedRows}
             onRowsPerPageChange={handleChangeRowsPerPage}
-    />
+        />
+     </ThemeProvider>  
     )
 }
 
